e12d26de3ad374628b5b9d258e9ddded820f385a
jls-github <souzajohnlee@gmail.com>
2020-06-03 13:45:39 -0700

    Setting up fetch requests for populating initial services feed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, {Frgament} from 'react';
+import React, {Fragment} from 'react';
+import ChurchServiceContainer from '/containers/ChurchServiceContainer'
 
 function App() {
   return (
     <Fragment>
       {/* Router for Admin Page - '/admin' */}
       {/* Router for Church Service page - '/' */}
+      <ChurchServiceContainer />
     </Fragment>
   );
 }

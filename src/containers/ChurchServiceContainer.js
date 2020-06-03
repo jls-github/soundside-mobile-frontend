e0464270d9518a677c75614221cde7ab3bf64f09
jls-github <souzajohnlee@gmail.com>
2020-06-03 14:04:07 -0700

    Setup fetch call for services index

--- src/containers/ChurchServiceContainer.js
+++ src/containers/ChurchServiceContainer.js
@@ -1,5 +1,5 @@
 import React, {Fragment} from 'react';
-import ServicesIndex from '/ServicesIndex'
+import ServicesIndex from './ServicesIndex'
 
 const ChurchServiceContainer = () => {
     return (

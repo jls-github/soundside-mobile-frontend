f2425c5a9c6a2fb90ea792489a6bed518001e61c
jls-github <souzajohnlee@gmail.com>
2020-06-03 13:37:42 -0700

    Basic frontend setup

---
+++ src/containers/ChurchServiceContainer.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+
+const ChurchServiceContainer = () => {
+    return (
+        <Fragment>
+            {/* Navbar */}
+            {/* Route for Services Index (if '/') */}
+            {/* Route for specific service (if /:date(5-31) or /:id) */}
+        </Fragment>
+    )
+}
+
+export default ChurchServiceContainer

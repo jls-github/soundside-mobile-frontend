291d5445a0ff5e162177956a8189a018d9cae762
jls-github <souzajohnlee@gmail.com>
2020-06-05 14:44:12 -0700

    Hooked up controlled forms for slides and sections - could be better managed with redux

--- src/adminPanelComponents/components/Slide.js
+++ src/adminPanelComponents/components/Slide.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {sortableElement} from 'react-sortable-hoc';
 import DragHandle from './DragHandle.js';
 
-const Slide = sortableElement(({title, content}) => {
+const Slide = sortableElement(({title, content, onSlideTitleChange, onSlideContentChange, id}) => {
     return(
         <li>
             <DragHandle />
-            {title}
-            {content}
+            <input type="text" value={title} onChange={(e => {onSlideTitleChange(e, id)})} />
+            <input type="text" value={content} onChange={(e => {onSlideContentChange(e, id)})} />
         </li>
     )
 })
--- src/adminPanelComponents/containers/Section.js
+++ src/adminPanelComponents/containers/Section.js
@@ -1,24 +1,30 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {sortableElement} from 'react-sortable-hoc';
-import arrayMove from 'array-move';
 import Slide from '../components/Slide.js';
 import DragHandle from '../components/DragHandle.js';
 import SortableContainer from '../components/SortableContainer.js'
 
-const Section = sortableElement(({title, oldSlides, id, onSectionTitleChange}) => {
-    
-    
-    const [slides, setSlides] = useState(oldSlides) //dummy state to start
-
-    console.log(oldSlides)
-
-    const onSortEnd = ({oldIndex, newIndex}) => { // do I need to take a picture of prev state before doing this?
-        setSlides(arrayMove(slides, oldIndex, newIndex))
-    }
+const Section = sortableElement(({
+    title, 
+    slides, 
+    id, 
+    onSectionTitleChange, 
+    onSlideSortEnd,
+    onSlideTitleChange,
+    onSlideContentChange
+    }) => {
 
     const populateSlides = () => {
         return slides.map((slide, index) => {
-            return <Slide key={`slide-${slide.id}`} index={index} title={slide.title} content={slide.content} />
+            return <Slide 
+                key={`slide-${slide.id}`}
+                id={slide.id}
+                index={index} 
+                title={slide.title} 
+                content={slide.content}
+                onSlideTitleChange={(e, slideId) => onSlideTitleChange(e, id, slideId)}
+                onSlideContentChange={(e, slideId) => onSlideContentChange(e, id, slideId)} 
+            />
         })
     }
 
@@ -26,8 +32,8 @@ const Section = sortableElement(({title, oldSlides, id, onSectionTitleChange}) =
         <li>
             <DragHandle />
             <input type="text" placeholder="Section Title" value={title} onChange={e => onSectionTitleChange(e, id)} />
-            <SortableContainer onSortEnd={onSortEnd} useDragHandle>
-                {slides ? populateSlides() : null}
+            <SortableContainer onSortEnd={(sortParams) => onSlideSortEnd(sortParams, id)} useDragHandle>
+                {slides ? populateSlides() : "loading..."}
             </SortableContainer>
         </li>
     )

0fc9a280d7dbe221d4aae89a669f5481a8d2745d
github username <github email address>
2020-06-08 12:05:15 -0700

    Styling

--- src/sundayComponents/components/Navbar.js
+++ src/sundayComponents/components/Navbar.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {Link} from 'react-router-dom'
 
 const Navbar = () => {
 
+    const navRef = useRef(null)
+
+    const handleNavbarSlide = () => {
+        navRef.current.classList.toggle("navbar-inactive") 
+        navRef.current.classList.toggle("navbar-active")
+    }
+
     return(
-        <ul>
-            <Link to="/church" >Services</Link>
-            <a href="https://soundsidechurch.com/discover.html">About Us</a>
-        </ul>
+        <div>
+            <ul className="navbar-button" onClick={handleNavbarSlide}>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        <div className="navbar navbar-inactive" ref={navRef}>
+            <ul>
+                <li><Link to="/church" >Services</Link></li>
+                <li><a href="https://soundsidechurch.com/discover.html">About Us</a></li>
+            </ul>
+        </div>
+        </div>
     )
 }
 
--- src/sundayComponents/containers/ChurchServiceContainer.js
+++ src/sundayComponents/containers/ChurchServiceContainer.js
@@ -2,26 +2,28 @@ import React, {Fragment} from 'react';
 import ServicesIndex from './ServicesIndex'
 import Slideshow from './Slideshow'
 import Navbar from '../components/Navbar'
+import '../sunday-service.sass'
+
 
 const ChurchServiceContainer = ({location}) => {
 
     const router = () => {
         const path = location.pathname.split("/")
-
         if (path[2]) {
             return <Slideshow serviceId={path[2]}/>
         } else {
             return <ServicesIndex />
         }
-
     }
 
     return (
         <Fragment>
 
-            <Navbar />
-
-            {router()}
+            <div className="sunday-service-container">
+                <div className="sunday-service-background"></div>
+                <Navbar />
+                {router()}
+            </div>
 
         </Fragment>
     )

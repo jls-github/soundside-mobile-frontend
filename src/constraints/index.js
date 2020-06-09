5dd0e1247f23d81dd178f5e4aa18f18c3136b69e
github username <github email address>
2020-06-09 14:20:35 -0700

    changed constraint URL to hosted backend url

--- src/constraints/index.js
+++ src/constraints/index.js
@@ -1,4 +1,4 @@
-export const APIROOT = "http://localhost:3000"
+export const APIROOT = "https://radiant-savannah-76771.herokuapp.com/"
 export const HEADERS = {
     "Content-Type": "application/json",
     Accept: "application/json",

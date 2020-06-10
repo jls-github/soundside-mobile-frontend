981d0afaf398d90f7043ee7ea780cc3152090b5f
github username <github email address>
2020-06-09 21:54:44 -0700

    Added styling for slide form

--- src/adminPanelComponents/components/Slide.js
+++ src/adminPanelComponents/components/Slide.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {sortableElement} from 'react-sortable-hoc';
-import DragHandle from './DragHandle.js';
 
 const Slide = sortableElement(({title, content, onSlideTitleChange, onSlideContentChange, id}) => {
     return(
         <li>
-            <DragHandle />
-            <input type="text" value={title} onChange={(e => {onSlideTitleChange(e, id)})} placeholder="Slide title" />
-            <input type="text" value={content} onChange={(e => {onSlideContentChange(e, id)})} placeholder="Slide Content" />
+            <div className="slide-container">
+                
+                <input type="text" value={title} onChange={(e => {onSlideTitleChange(e, id)})} placeholder="Slide title (optional)" />
+                <br />
+                <textarea value={content} onChange={(e => {onSlideContentChange(e, id)})} placeholder="Slide Content" />
+            </div>
         </li>
     )
 })

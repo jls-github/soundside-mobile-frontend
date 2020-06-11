f72f24ad9038f20ed7f191552fa402bd16b8307b
jls-github <souzajohnlee@gmail.com>
2020-06-11 01:20:25 -0700

    Added darker green styling

--- src/sundayComponents/containers/ServicesIndex.js
+++ src/sundayComponents/containers/ServicesIndex.js
@@ -7,7 +7,6 @@ const ServicesIndex = (props) => {
 
     return(
         <div className="services-index">
-            <div className="services-header">Engaging Others with the Love of Christ<br />That They Might Enjoy New Life in Christ</div>
             <div className="services-list">
                 <Service />
             </div>

729efb9482d4878ff0f33bf52e51e205d1f91fc1
jls-github <souzajohnlee@gmail.com>
2020-06-10 23:12:53 -0700

    Changed service display to only display one service

--- src/sundayComponents/containers/ServicesIndex.js
+++ src/sundayComponents/containers/ServicesIndex.js
@@ -4,30 +4,12 @@ import Service from '../components/Service.js'
 
 const ServicesIndex = (props) => {
 
-    const [services, setServices] = useState()
-
-    const populateServices = () => {
-        return services.map(service => {
-            return <Service date={service.date} key={service.id} id={service.id} />
-        })
-    }
-
-    useEffect(() => {
-
-        async function fetchServices() {
-            let response = await fetch(APIROOT + '/services')
-            let json = await response.json()
-            await setServices(json)
-        }
-
-        fetchServices()
-    }, [])
 
     return(
         <div className="services-index">
             <div className="services-header">Engaging Others with the Love of Christ<br />That They Might Enjoy New Life in Christ</div>
             <div className="services-list">
-                {services ? populateServices() : "loading..."}
+                <Service />
             </div>
         </div>
     )

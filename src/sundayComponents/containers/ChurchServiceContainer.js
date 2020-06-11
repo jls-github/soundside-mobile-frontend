3dcbb9f702396c78ac5591c06169456dc9871af2
jls-github <souzajohnlee@gmail.com>
2020-06-10 21:29:36 -0700

    more modern styling option

--- src/sundayComponents/containers/ChurchServiceContainer.js
+++ src/sundayComponents/containers/ChurchServiceContainer.js
@@ -3,6 +3,7 @@ import ServicesIndex from './ServicesIndex'
 import Slideshow from './Slideshow'
 import Navbar from '../components/Navbar'
 import '../sunday-service.sass'
+import BackgroundImage from '../../images/background_image.jpg'
 
 
 const ChurchServiceContainer = ({location}) => {
@@ -21,6 +22,7 @@ const ChurchServiceContainer = ({location}) => {
 
             <div className="sunday-service-container">
                 <div className="sunday-service-background"></div>
+                <img className="sunday-service-picture" src={BackgroundImage}/>
                 <Navbar />
                 <div className="main-content-container">
                 {router()}

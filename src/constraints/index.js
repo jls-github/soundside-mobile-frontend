8ca76e152030192f6e2864ee449b9e1ae497a37e
jls-github <souzajohnlee@gmail.com>
2020-06-11 12:20:13 -0700

    fixed api root constraint

--- src/constraints/index.js
+++ src/constraints/index.js
@@ -1,4 +1,4 @@
-export const APIROOT = "https://radiant-savannah-76771.herokuapp.com/"
+export const APIROOT = "https://radiant-savannah-76771.herokuapp.com"
 export const HEADERS = {
     "Content-Type": "application/json",
     Accept: "application/json",

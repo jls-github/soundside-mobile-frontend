2163be5eb5da6bf1181792953344833857048ade
github username <github email address>
2020-06-15 16:04:17 -0700

    Whole slide moves now instead of opacity fade

--- src/sundayComponents/components/Slide.js
+++ src/sundayComponents/components/Slide.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import {useSwipeable} from 'react-swipeable'
 
 
 const Slide = (props) => {
 
     const {title, content} = props.slide
+    const {onNextSlide, onPreviousSlide} = props
+
+    const handlers = useSwipeable({
+        onSwipedLeft: (e) => onNextSlide(),
+        onSwipedRight: (e) => onPreviousSlide()
+    })
 
     return(
-
-        <div className="slide">
-            <div className="slide-title">{title}</div>
-            <div className="slide-content">{content}</div>
+        <div className="slide-container" {...handlers}>
+            <div className="slide">
+                <div className="slide-title">{title}</div>
+                <div className="slide-content">{content}</div>
+                <div className="slide-buttons">
+                    <button className="right-button" onClick={onNextSlide}><div className="button-overlay">{">"}</div></button>
+                    <button className="left-button" onClick={onPreviousSlide}><div className="button-overlay">{"<"}</div></button>
+                </div>
+                <div><span></span><span></span><span></span></div>
+            </div>
         </div>
     )
 }

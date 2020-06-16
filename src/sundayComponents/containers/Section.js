0f5b4be4f0566f40d5dfe853414e3d245573e481
jls-github <souzajohnlee@gmail.com>
2020-06-15 18:47:55 -0700

    added back Section.js

---
+++ src/sundayComponents/containers/Section.js
@@ -0,0 +1,26 @@
+  
+// import React from 'react';
+// import Slide from '../components/Slide.js'
+
+
+// THIS FILE IS NO LONGER BEING USED...
+
+// const Section = (props) => {
+
+//     const {title, slides} = props.section
+
+//     const populateSlides = (slides) => { 
+//         return slides.map(slide => {
+//             return <Slide slide={slide} key={slide.id}/>
+//         })
+//     }
+
+//     return (
+//         <div>
+//             <h2>{title}</h2>
+//             {slides ? populateSlides(slides) : null}
+//         </div>
+//     )
+// }
+
+// export default Section

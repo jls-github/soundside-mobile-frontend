2791e21af2476890444607acce2587aa15a8320d
John <souzajohnlee@gmail.com>
2021-08-24 13:34:14 -0700

    remove unused hooks

--- src/sundayComponents/containers/ChurchServiceContainer.js
+++ src/sundayComponents/containers/ChurchServiceContainer.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment } from "react";
 import { useLocation } from "react-router-dom";
 import ServicesIndex from "./ServicesIndex";
 import Slideshow from "./Slideshow";

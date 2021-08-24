57684d972ce6fc5da0d1daae8d1d108c3a6efe50
John <souzajohnlee@gmail.com>
2021-08-24 13:15:29 -0700

    add installation instructions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Fragment} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import ReactPWAInstallProvider from "react-pwa-install"
 import ChurchServiceContainer from './sundayComponents/containers/ChurchServiceContainer'
 import ChurchRedirect from './sundayComponents/components/ChurchRedirect.js'
 import AdminPanelContainer from './adminPanelComponents/containers/AdminPanelContainer.js'
@@ -15,9 +14,7 @@ function App() {
         <Switch>
           <Route path="/admin" component={AdminPanelContainer} />
           <Route path="/church">
-            <ReactPWAInstallProvider>
               <ChurchServiceContainer />
-            </ReactPWAInstallProvider>
           </Route>
           <Route path="*" component={ChurchRedirect} />
         </Switch>
--- src/sundayComponents/containers/ChurchServiceContainer.js
+++ src/sundayComponents/containers/ChurchServiceContainer.js
@@ -1,68 +1,63 @@
-import React, {Fragment} from 'react';
-import useReactPWAInstall from "react-pwa-install";
-import {useLocation} from 'react-router-dom'
-import ServicesIndex from './ServicesIndex'
-import Slideshow from './Slideshow'
-import Navbar from '../components/Navbar'
-import '../sunday-service.sass'
-import BackgroundImage from '../../images/background_image.jpg'
-import ConnectionCard from './ConnectionCard.js';
-
+import React, { Fragment, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import ServicesIndex from "./ServicesIndex";
+import Slideshow from "./Slideshow";
+import Navbar from "../components/Navbar";
+import "../sunday-service.sass";
+import BackgroundImage from "../../images/background_image.jpg";
+import ConnectionCard from "./ConnectionCard.js";
 
 const ChurchServiceContainer = () => {
+  const [showInstallPrompt, setShowInstallPrompt] = useState(false)
+  const [deferredPrompt, setDeferredPrompt] = useState(null)
 
-    const location = useLocation()
+  const location = useLocation();
 
-    const router = () => {
-        const path = location.pathname.split("/")
-        if (path[2] === "connect")  {
-            return <ConnectionCard />
-        } else if (path[2]) {
-            return <Slideshow serviceId={path[2]}/>
-        } else {
-            return <ServicesIndex />
-        }
+  const router = () => {
+    const path = location.pathname.split("/");
+    if (path[2] === "connect") {
+      return <ConnectionCard />;
+    } else if (path[2]) {
+      return <Slideshow serviceId={path[2]} />;
+    } else {
+      return <ServicesIndex />;
     }
+  };
 
-    const { pwaInstall, supported, isInstalled } = useReactPWAInstall();
- 
-    const handleClick = () => {
-      pwaInstall({
-        title: "Install Web App",
-        logo: myLogo,
-        features: (
-          <ul>
-            <li>Cool feature 1</li>
-            <li>Cool feature 2</li>
-            <li>Even cooler feature</li>
-            <li>Works offline</li>
-          </ul>
-        ),
-        description: "This is a very good app that does a lot of useful stuff. ",
-      })
-        .then(() => alert("App installed successfully or instructions for install shown"))
-        .catch(() => alert("User opted out from installing"));
-    };
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      setDeferredPrompt(e)
+      // Update UI notify the user they can install the PWA
+      setShowInstallPrompt(true)
+      // Optionally, send analytics event that PWA install promo was shown.
+      console.log(`'beforeinstallprompt' event was fired.`);
+    });
+  }, []);
 
-    return (
-        <Fragment>
+  const handleClickInstall = async () => {
+    setShowInstallPrompt(false)
+    deferredPrompt.prompt()
+    const { outcome } = await deferredPrompt.userChoice;
+    // Optionally, send analytics event with outcome of user choice
+    console.log(`User response to the install prompt: ${outcome}`);
+    // We've used the prompt, and can't use it again, throw it away
+    setDeferredPrompt(null);
+  }
 
-            <div className="sunday-service-container">
-                <div className="sunday-service-background"></div>
-                <img className="sunday-service-picture" src={BackgroundImage} alt=""/>
-                <Navbar />
-                <div className="main-content-container">
-                    {router()}
-                </div>
-                {supported() && !isInstalled() && (
-                    <button type="button" onClick={handleClick}>
-                        Install App
-                    </button>
-                )}
-            </div>
+  return (
+    <Fragment>
+      <div className="sunday-service-container">
+        <div className="sunday-service-background"></div>
+        <img className="sunday-service-picture" src={BackgroundImage} alt="" />
+        <Navbar />
+        <div className="main-content-container">{router()}</div>
+      </div>
+      {showInstallPrompt && <button onClick={handleClickInstall}>Install</button>}
+    </Fragment>
+  );
+};
 
-        </Fragment>
-    )
-}
-
-export default ChurchServiceContainer
+export default ChurchServiceContainer;
